e8bf6d8269a282c4d5b11aba523bbc8b0d290634
wyl3waK <82113302+wyl3waK@users.noreply.github.com>
2021-06-19 16:40:10 -0700

    not-really-math code

---
+++ algo/not-really-math/not-really-math code.py
@@ -0,0 +1,34 @@
+# since addition comes b4 multiplication, i split the characters by multiplication
+# 1. split the problem by multplication 
+# 2. calculate the addition part and replace it
+# 3. calculate the product
+# 4. take the mod of it
+# addition part looks like 6a8 or 3a5a5a4
+
+def multiplyList(myList): #copied and pasted cuz too lazy lol
+    result = 1
+    for x in myList:
+         result = result * x
+    return result
+
+problem = "3a5a5a4" # get the thingy that needs to be calculated
+problem = problem.split("m") # split the problem by multiplication
+print(problem) # you get each part in a separate item (addition parts are all separated)
+for i in problem: 
+	if "a" in i: 
+		j = i.split("a") # split the numbers in the addition part
+		print(j)
+		for k in range(len(j)): # this just converts each item in the list j from string to integer
+			j[k] = int(j[k])
+		summ = sum(j) #sum() is a built in python thing that calculates the sum of a list
+		problem[problem.index(i)] = str(summ) # this replaces the addition part with the sum
+
+print("no more addition parts: " + str(problem)) # now all the addition parts are replaced with sums
+for k in range(len(problem)): # again, converts each item from string to integer
+	problem[k] = int(problem[k]) 
+
+product = multiplyList(problem) # multiplies all the values in the list
+print(product)
+if product > 2**32 - 2: # takes the mod if its over 2**32 -1
+	product = product % (2**32 - 1)
+print(product)
